e5ed626df04f02bbf0acc76b86b185aa0def6283
Katharine Cordes <katharinegillis@users.noreply.github.com>
2021-05-26 10:25:29 -0600

    refactor: fix install to not exit with error

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 [ ! -d "$HOME/utils" ] && mkdir $HOME/utils
-cd $HOME/utils || echo "no utils folder"
+cd $HOME/utils || return
 
 # Download the adminer docker
 git clone git@github.com:katharinegillis/docker-adminer.git adminer
 
 # Start up adminer
-cd $HOME/utils/adminer || "no utils/adminer folder"
+cd $HOME/utils/adminer || return
 docker-compose up -d
